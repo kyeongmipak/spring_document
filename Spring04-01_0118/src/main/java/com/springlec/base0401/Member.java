b314d3a461e5e60ec69dc193293a483b0c3faf40
kyeongmipak <qkrrudal7@naver.com>
2021-01-18 18:21:16 +0900

    1/18_bean 사용하여 data 전달_kyeongmi

---
+++ Spring04-01_0118/src/main/java/com/springlec/base0401/Member.java
@@ -0,0 +1,61 @@
+package com.springlec.base0401;
+
+// bean
+public class Member {
+
+	// Fields
+	private String name;
+	private String id;
+	private String pw;
+	private String email;
+	
+	
+	//constructor
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getPw() {
+		return pw;
+	}
+
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
